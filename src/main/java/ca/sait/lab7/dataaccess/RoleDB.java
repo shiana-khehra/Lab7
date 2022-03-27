8207fe47a0b99da986950b303e7f683b232caef8
shiana-khehra <97817879+shiana-khehra@users.noreply.github.com>
2022-03-26 19:51:49 -0600

    Removed unnecessary imports in RoleDB.java

--- src/main/java/ca/sait/lab7/dataaccess/RoleDB.java
+++ src/main/java/ca/sait/lab7/dataaccess/RoleDB.java
@@ -1,10 +1,6 @@
 package ca.sait.lab7.dataaccess;
 
 import ca.sait.lab7.models.Role;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -17,17 +13,6 @@ public class RoleDB {
         try {
             Query query = em.createNamedQuery("Role.findAll");
             return query.getResultList();
-        } finally {
-            em.close();
-        }
-    }
-
-    public int getRoleId(String roleName) throws Exception{
-        EntityManager em = DBUtil.getEmFactory().createEntityManager();
-        
-        try {
-            Query query = em.createNamedQuery("Role.findByRoleName");
-            return query.getFirstResult();
         } finally {
             em.close();
         }
